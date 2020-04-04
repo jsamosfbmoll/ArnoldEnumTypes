e555acc8f300cfcf160b22a7e0d7af0aa2942da8
Jose Maria Samos <jsamos@cifpfbmoll.eu>
2020-04-04 21:57:26 +0200

    docs(ArnoldMain.java): Descomentar código
    Descomentar código que muestra tu peso en planetas terrestres
    y gaseosos por separado

--- src/main/java/edu/elsmancs/enumTypes/ArnoldMain.java
+++ src/main/java/edu/elsmancs/enumTypes/ArnoldMain.java
@@ -11,7 +11,7 @@ public class ArnoldMain {
          for(Planeta planeta : Planeta.values()){
         	 System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
          }
-         /*
+         
          System.out.println("\nYour weight only on the terrestrial planets: ");
          for(Planeta planeta: Planeta.getPlanetasTerrestres()){
         	 System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
@@ -20,7 +20,7 @@ public class ArnoldMain {
          System.out.println("\nYour weight only on the gas giant planets: ");
          for(Planeta planeta: Planeta.getGigantesGaseosos()){
         	 System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
-         }*/
+         }
 	 }
 
 }
